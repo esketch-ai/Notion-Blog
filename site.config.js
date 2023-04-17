56beab74dfefa2cf40da0591163e883ca5d6ce0d
esketch <32926194+esketch-ai@users.noreply.github.com>
2023-04-17 17:48:35 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "Esketch Song",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/SSH_Profile.jpeg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "General AI planning and development",
     bio: "AI planning and development",
     email: "esketch@gmail.com",
