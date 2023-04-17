8748843319285ebb816afd17771ce0be491ab5aa
esketch <32926194+esketch-ai@users.noreply.github.com>
2023-04-17 17:24:22 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -25,11 +25,11 @@ const CONFIG = {
 
   // CONFIG configration (required)
   link: "https://esketch-ai.vercel.app",
-  since: 2022, // If leave this empty, current year will be used.
+  since: 2023, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
   seo: {
-    keywords: ["Blog", "Website", "Notion"],
+    keywords: ["AI", "Pythob", "Security", "Abnormal Detect", "Blog", "Website", "Notion"],
   },
 
   // notion configuration (required)
